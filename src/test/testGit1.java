00b5f9910d176c682b18bf4145614814a8344b8a
limbirdy74 <limbirdy@naver.com>
2024-11-14 15:16:23 +0900

    자바 github 소스 수정 테스트

--- src/test/testGit1.java
+++ src/test/testGit1.java
@@ -5,6 +5,7 @@ public class testGit1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git hub test");
+		System.out.println("git hub test222");
 	}
 
 }
